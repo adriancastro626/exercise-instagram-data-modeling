dce2df2a490799d10c7bab5952e1a4162b800e39
adriancastro626 <adriancastro626@icloud.com>
2021-04-01 03:04:09 +0000

    resolved

--- src/models.py
+++ src/models.py
@@ -1,12 +1,12 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 import enum
-from sqlalchemy import Integer, Enum
+
 
 Base = declarative_base()
 
@@ -45,24 +45,6 @@ class Media(Base):
     url = Column(String(250), nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'))    
     person = relationship(Post)
-
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
 
     def to_dict(self):
         return {}
